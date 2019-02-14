beeb90f6bd726e3c927d4f5a797a2467595ba1cc
Nate Harris <nathanharris.sf@gmail.com>
2019-02-13 23:25:18 -0800

    Add todos controller

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Todo;
+
+class TodosController extends Controller
+{
+    public function index()
+    {
+        $todos = Todo::all();
+
+        return view('welcome', compact('todos'));
+    }
+}
