a7238aca621ade1b6036d2f3cbcbe6e40eebbf67
Nate Harris <nathanharris.sf@gmail.com>
2019-02-13 23:27:16 -0800

    Route to todos controller

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,8 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+
+Artisan::command('hello', function () {
+    $this->comment('Hi there, you\'re doing great!');
+})->describe('Just checking in');
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TodosController@index');
 
 Route::get('/about', function () {
     return view('about');
