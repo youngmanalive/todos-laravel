8630f5d893b03a3dbd8e61c1328a0c28d5242a95
Nate Harris <nathanharris.sf@gmail.com>
2019-02-14 20:18:54 -0800

    Update nav

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,14 +4,14 @@
   <meta charset="utf-8" />
   <title>Todos @yield('title')</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://bootswatch.com/4/cosmo/bootstrap.min.css">
   <style>
     
     .nav { color: white; }
     .nav * { color: inherit; }
-    .fill { height: 90vh; }
+    .minw-4 { min-width: 400px; }
 
   </style>
-  <link rel="stylesheet" href="https://bootswatch.com/4/cosmo/bootstrap.min.css">
 </head>
 <body>
   <nav class="navbar navbar-dark bg-primary">
@@ -19,11 +19,9 @@
       <span class="navbar-brand font-weight-bold">Todos!</span>
       <ul class="nav">
         <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="/manage">Manage</a></li>
-        <li class="nav-item"><a class="nav-link" href="/new">New</a></li>
+        <li class="nav-item"><a class="nav-link" href="/todos/new">New</a></li>
         <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
-        <li class="nav-item"><a class="nav-link" href="/app">App</a></li>
-        <li class="nav-item"><a class="nav-link" href="/sample">Sample</a></li>
+        <li class="nav-item"><a class="nav-link" href="/app"><strong class="ml-1">Try the App</strong></a></li>
       </ul>
     </div>
   </nav>
