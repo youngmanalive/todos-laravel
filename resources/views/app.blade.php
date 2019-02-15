a5542e71967eeda9f718198366f1f99a1f8d94ac
Nate Harris <nathanharris.sf@gmail.com>
2019-02-15 15:42:43 -0800

    Clean up webpackx

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,13 +2,15 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>Todos - App</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-  <script src="{{ asset('js/app.js') }}"></script>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
   <div id="root"></div>
+  <script src="{{ asset('js/manifest.js') }}"></script>
+  <script src="{{ asset('js/vendor.js') }}"></script>
+  <script src="{{ asset('js/bundle.js') }}"></script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.react('resources/js/app.js', 'public/js')
+mix.react('resources/js/app.js', 'public/js/bundle.js')
+   .extract(['react'])
    .sass('resources/sass/app.scss', 'public/css');
