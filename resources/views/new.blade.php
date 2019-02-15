da7c7b2be3fd191e8133ab1648bae59f3668d6c8
Nate Harris <nathanharris.sf@gmail.com>
2019-02-14 20:19:24 -0800

    Working todo create form

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -3,5 +3,24 @@
 @section('title', '- New')
     
 @section('content')
-    <h1>Add a Todo!</h1>
+    <h1>New</h1>
+    <div class="container p-4">
+        <div class="card w-50">
+            <span class="card-header">Add a Todo!</span>
+            <div class="container p-4">
+                <form method="POST" action="/">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="title"><strong>To Do:</strong></label>
+                        <input name="title" type="text" class="form-control" id="title" placeholder="What needs done?" maxlength="60">
+                    </div>
+                    <div class="form-group">
+                        <label for="desc">Description</label>
+                        <textarea name="description" class="form-control" id="desc" rows=4 maxlength="200"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
