fc4390106921fc38b076e5bbf7161d5d31f33adf
Nate Harris <nathanharris.sf@gmail.com>
2019-02-14 20:16:40 -0800

    Todo crud methods

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -11,6 +11,55 @@ class TodosController extends Controller
     {
         $todos = Todo::all();
 
-        return view('welcome', compact('todos'));
+        return view('home', compact('todos'));
+    }
+
+    public function create()
+    {
+        return view('new');
+    }
+
+    public function store()
+    {
+        $newTodo = new Todo;
+
+        $newTodo->title = request('title');
+        $newTodo->description = request('description');
+
+        $newTodo->save();
+
+        return redirect('/');
+    }
+
+    public function show($id)
+    {
+        $todo = Todo::find($id);
+        
+        return view('show', compact('todo'));
+    }
+
+    public function edit($id)
+    {
+        $todo = Todo::find($id);
+
+        return view('edit', compact('todo'));
+    }
+
+    public function destroy($id)
+    {
+        return dd("You are destroying ID: $id!");
+    }
+
+    public function update($id)
+    {   
+        $todo = Todo::find($id);
+
+        $todo->completed = request()->has('completed');
+        $todo->title = request('title');
+        $todo->description = request('description');
+
+        $todo->save();
+        
+        return redirect("/todos/$id");
     }
 }
