b311f743b6462b1ace2ee0b8f3fb124bfdd86cc3
Nate Harris <nathanharris.sf@gmail.com>
2019-02-17 19:08:00 -0800

    Add todostore actions and computeds

--- resources/js/stores/todosStore.js
+++ resources/js/stores/todosStore.js
@@ -1,14 +1,29 @@
-import { observable, action, autorun } from "mobx";
+import { observable, action, computed } from "mobx";
+import TodosAPI from "../api/todosAPI";
 
 class TodosStore {
-  @observable todos = [
-    { title: "Do something" },
-    { title: "Do something else" }
-  ];
+  @observable todos = {};
+  todosAPI = new TodosAPI;
 
-  @action.bound addTodo(obj) {
-    this.todos.push(obj);
-  }
+  @computed get todoList() {
+    return Object.values(this.todos);
+  };
+
+  @action fetchTodos = () =>
+    this.todosAPI.fetchTodos()
+      .then(todos => todos.map(todo => this.todos[todo.id] = todo));
+
+  @action addTodo = obj => 
+    this.todosAPI.createTodo(obj)
+      .then(todo => this.todos[todo.id] = todo);
+  
+  @action updateTodo = obj =>
+    this.todosAPI.updateTodo(obj)
+      .then(todo => this.todos[todo.id] = todo);
+
+  @action deleteTodo = id =>
+    this.todosAPI.deleteTodo(id)
+      .then(() => delete this.todos[id]);
 }
 
 export default new TodosStore;
